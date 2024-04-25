046e8180422d260f22e7f0528bbaadd61dd73873
Thoughtwin <azharrazakhan78611>
2024-04-25 18:46:39 +0530

    add project on git

---
+++ src/component/Forgetpassword/ForgetPassword.jsx
@@ -0,0 +1,88 @@
+import { NavLink,useNavigate } from "react-router-dom";
+import { useState } from "react";
+import { useFormik } from "formik";
+import { forgetPasswordValidation } from "../validation/formValistion";
+import {postMethodData} from "../../Service/apiServie.js"
+import  '../../App.css';
+
+export default function Forgetpassword() {
+    const [isLoading,setIsLoading] = useState(false);
+
+
+  const redirect = useNavigate()
+
+
+
+  const {handleSubmit,handleChange,handleBlur,errors} = useFormik({
+    initialValues:{email:''},
+    validationSchema:forgetPasswordValidation,
+    onSubmit:(value) => {
+      console.log(value)
+      const result = postMethodData('http://localhost:3000/sendMail','POST',value)
+    result.then(json => {
+      setIsLoading(true);
+      if(Object.keys(json).length > 0) {    
+        console.log(Object.keys(json))
+        setTimeout(() => {
+          setIsLoading(false);
+          alert('Mail is Send on your Email Address')
+                }, 2000);
+
+      }})
+    },
+  
+    
+  })
+
+
+if (isLoading) {
+  return <><div className="loader"></div></>;
+}
+
+
+
+
+  
+
+    return (
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-10 w-auto"                             src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+          alt="Your Company" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Forget Password</h2>
+          <p>Just enter your email and we will send you  a reset password link.</p>
+        </div>
+        
+        <div className="mt-5 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form onSubmit={handleSubmit} className="space-y-6" action="#" method="POST">
+            <div>
+              <label htmlFor="email" className="block font-medium leading-6 text-gray-900">Email address</label>
+              <div className="mt-2">
+                <input id="email"  
+                name="email"  
+                type="email" 
+                onChange={handleChange} 
+                onBlur={handleBlur}
+                autoComplete="off" 
+                className={`block w-full p-2 rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset 
+                ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                 focus:ring-orange-600 sm:leading-6 ${errors.email ? 'error-border' :''}`} />
+              </div>
+              {errors.email && <span className="text-red-600">{errors.email}</span>}
+
+            </div>
+
+            <div>
+              <button type="submit" className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Send Recovery Mail</button>
+            </div>
+          </form>
+        
+          <p className="mt-10 text-center text-gray-500">
+            Not a member?
+            <NavLink to="/signup" className="font-semibold leading-6 text-orange-600 hover:text-orange-500">Sign Up</NavLink>
+          </p>
+        </div>
+      </div>
+    );
+  }
+  
---
+++ src/component/Signin/Signin.jsx
@@ -0,0 +1,242 @@
+import { useState } from "react";
+import { NavLink,useNavigate } from "react-router-dom";
+import {useFormik} from "formik";
+import { signInValidations } from "../validation/formValistion";
+import {postMethodData} from "../../Service/apiServie.js";
+import  '../../App.css';
+
+export default function Signin() {
+  const [isLoading,setIsLoading] = useState(false);
+  //using Formik
+  const initialState = {
+    email: '',
+    password: ''
+  } 
+
+  const redirect = useNavigate()
+
+  const {values,handleBlur,handleChange,handleSubmit,errors} = useFormik({
+    initialValues:initialState,
+    validationSchema:signInValidations,
+    onSubmit:values=> {
+    const response =  postMethodData('http://localhost:3000/auth/sign_in','POST',values)
+    response.then(json => {
+      console.log(response);
+      setIsLoading(true);
+          if(json && json.token.length > 0) {    
+            setTimeout(() =>{
+              setIsLoading(false);
+              redirect('/dashboard')
+            },2000 )
+            localStorage.setItem('token', json.token)
+          }
+        }
+      )
+    response.catch((errors) => {console.log(errors)})
+    } })
+
+    if (isLoading) {
+      return <><div className="loader"></div></>;
+    }
+    
+
+//Normal Form
+  // const [email,setEmail] = useState('')
+  // const [password,setPassword] = useState('')
+
+  // const handleSubmit = (e) =>{
+  //   e.preventDefault()
+  //   console.log(email,password)   
+  //   // axios.post("http://localhost:5000/user", {
+  //   //         name,
+  //   //         email
+  //   //         // header
+  //   //     }).then(redirect("/read")).catch((err) => console.log(err))
+
+  // }
+
+
+  // const [formData,setFormData] = useState({
+  //   email:'',
+  //   password:''
+  // })
+
+  // const [error,setError] = useState({})
+    
+  // const handleInput = (e) => {
+  //   const name = e?.target?.name
+  //   const value = e?.target?.value
+  //   setFormData({
+  //     ...formData,
+  //     [name]:value
+  //   })
+
+
+  //   const validationError = {};
+  //   if (!formData?.email.trim()) {
+  //     validationError.email = 'Email is required';
+  //   } else if (!/\S+@\S+\.\S+/.test(formData.email) && formData.email.length > 0) {
+  //     validationError.email = 'Invalid email address';
+  //   }
+  
+  //   if (!formData?.password.trim()) {
+  //     validationError.password = 'Password is required';
+  //   } else if (formData?.password.length < 6) {
+  //     validationError.password = 'Password must be at least 6 characters';
+  //   }
+  
+  //   setError(validationError);
+  // }
+
+  // const handleSubmit = (e) => {
+   
+  //   e.preventDefault();
+  
+  //   const validationError = {};
+  //   if (!formData?.email.trim()) {
+  //     validationError.email = 'Email is required';
+  //   } else if (!/\S+@\S+\.\S+/.test(formData.email) && formData.email.length > 1) {
+  //     validationError.email = 'Invalid email address';
+  //   }
+  
+  //   if (!formData?.password.trim()) {
+  //     validationError.password = 'Password is required';
+  //   } else if (formData?.password.length < 6) {
+  //     validationError.password = 'Password must be at least 6 characters';
+  //   }
+  
+  //   setError(validationError);
+  
+  //   if (Object.keys(validationError).length === 0) {
+  //     alert('Form submit Success');
+  //     console.log(formData);
+  //     setFormData('')
+
+  //   }
+  // };
+  //Use Formi
+  return (
+      <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+        <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+          <img className="mx-auto h-10 w-auto"        
+            src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+          alt="Your Company" />
+          <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+        </div>
+        
+        <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+          <form className="space-y-6" onSubmit={handleSubmit}>
+            <div>
+              <label htmlFor="email" className="block font-medium leading-6 text-gray-900">Email address</label>
+              <div className="mt-2">
+          <input id="email" name="email" 
+           onChange={handleChange}
+           onBlur={handleBlur}
+           value={values.email}
+
+
+            type="email" 
+            autoComplete="off" 
+          className={`block w-full rounded-md p-2 border-0 py-1.5
+           text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+            placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+             focus:ring-orange-600 sm:text-sm sm:leading-6 ${errors.email ? 'error-border' : ''}`}
+          />
+              </div>
+              {errors.email && <span className="text-red-600">{errors.email}</span>}
+            </div>
+
+            
+        
+            <div>
+              <div className="flex items-center justify-between">
+                <label htmlFor="password" className="block font-medium leading-6 text-gray-900">Password</label>
+                <div className="">
+                  <NavLink to="/forget" className="font-semibold text-orange-600 hover:text-indigo-500">Forgot password?</NavLink>
+                </div>
+              </div>
+              <div className="mt-2">
+                <input id="password" name="password" 
+                onChange={handleChange}
+                onBlur={handleBlur}
+                value={values.password} 
+                type="password" 
+                border={
+                 "1px solid red"
+                }
+                autoComplete="current-password"  
+                className={`block w-full rounded-md border-0 py-1.5 p-2
+                 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300
+                  placeholder:text-gray-400 focus:ring-2 focus:ring-inset
+                   focus:ring-orange-600 sm:leading-6 ${errors.password ? 'error-border':'' }`} />
+              </div>
+              {errors.password && <span className="text-red-600">{errors.password}</span>}
+            </div>
+        
+            <div>
+              <button type="submit" className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Sign in</button>
+            </div>
+          </form>
+        
+          <p className="mt-10 text-center text-gray-500">
+            Not a member?
+            <NavLink to="/signup" className="font-semibold leading-6 text-orange-600 hover:text-orange-500">Sign Up</NavLink>
+          </p>
+        </div>
+      </div>
+    ); 
+  
+
+  
+
+
+    // return (
+    //   <div className="flex min-h-full flex-col justify-center px-6 py-12 lg:px-8">
+    //     <div className="sm:mx-auto sm:w-full sm:max-w-sm">
+    //       <img className="mx-auto h-10 w-auto"        
+    //         src="https://alexharkness.com/wp-content/uploads/2020/06/logo-2.png"
+    //       alt="Your Company" />
+    //       <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-gray-900">Sign in to your account</h2>
+    //     </div>
+        
+    //     <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
+    //       <form className="space-y-6" onSubmit={formik.onSubmit}>
+    //         <div>
+    //           <label htmlFor="email" className="block text-sm font-medium leading-6 text-gray-900">Email address</label>
+    //           <div className="mt-2">
+    //       <input id="email" name="email"  onChange={handleInput} type="email" autoComplete="off" 
+    //       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6" 
+    //       />
+    //           </div>
+    //           {error.email && <span className="text-red-600">{error.email}</span>}
+    //         </div>
+
+            
+        
+    //         <div>
+    //           <div className="flex items-center justify-between">
+    //             <label htmlFor="password" className="block text-sm font-medium leading-6 text-gray-900">Password</label>
+    //             <div className="text-sm">
+    //               <NavLink to="/forget" className="font-semibold text-orange-600 hover:text-indigo-500">Forgot password?</NavLink>
+    //             </div>
+    //           </div>
+    //           <div className="mt-2">
+    //             <input id="password" name="password" onChange={handleInput} type="password" autoComplete="current-password"  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6" />
+    //           </div>
+    //           {error.password && <span className="text-red-600">{error.password}</span>}
+    //         </div>
+        
+    //         <div>
+    //           <button type="submit" className="flex w-full justify-center rounded-md bg-orange-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-orange-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-orange-600">Sign in</button>
+    //         </div>
+    //       </form>
+        
+    //       <p className="mt-10 text-center text-sm text-gray-500">
+    //         Not a member?
+    //         <NavLink to="/signup" className="font-semibold leading-6 text-orange-600 hover:text-orange-500">Sign Up</NavLink>
+    //       </p>
+    //     </div>
+    //   </div>
+    // );
+  }
+  
